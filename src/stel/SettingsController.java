aadb835bd76f646c51bfb06eed42885f99923529
stelro <stelmach.ro@gmail.com>
2016-05-30 18:57:37 +0300

    load bucketname from file

--- src/stel/SettingsController.java
+++ src/stel/SettingsController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -18,9 +19,13 @@ public class SettingsController {
     @FXML private Button closeButton;
     @FXML private PasswordField accessKeyField;
     @FXML private PasswordField securityKeyField;
+    @FXML private TextField bucketName;
     @FXML private Label labelFile;
-    private String path;
-    private File file;
+    private String pathCredentials;
+    private String pathBucket;
+    private File fileCredentials;
+    private File fileBucket;
+    private boolean settingsExsists;
     private static String OS = System.getProperty("os.name").toLowerCase();
     java.util.Properties properties = System.getProperties();
 
@@ -33,28 +38,45 @@ public class SettingsController {
 
         if (isWindows()) {
 
-            path = "C:" + File.separator + "Users" + File.separator + System.getProperty("user.name") + File.separator + ".aws" +
+            pathCredentials = "C:" + File.separator + "Users" + File.separator + System.getProperty("user.name") + File.separator + ".aws" +
                     File.separator + "credentials";
+            pathBucket = "C:" + File.separator + "Users" + File.separator + System.getProperty("user.name") + File.separator + ".aws" +
+                    File.separator + "bucketname";
+
             MainModel.getInstance().print("Operating System : Windows");
 
         } else if (isUnix()) {
 
-            path =  properties.get("user.home").toString() + properties.get("file.separator").toString()  + ".aws" +
+            pathCredentials =  properties.get("user.home").toString() + properties.get("file.separator").toString()  + ".aws" +
                     properties.get("file.separator").toString()  + "credentials";
+            pathBucket =  properties.get("user.home").toString() + properties.get("file.separator").toString()  + ".aws" +
+                    properties.get("file.separator").toString()  + "bucketname";
+
             MainModel.getInstance().print("Operating System : Unix/Linux");
 
 
         }
 
-         file = new File(path);
+        fileCredentials = new File(pathCredentials);
+        fileBucket = new File(pathBucket);
 
-        if(file.exists() && !file.isDirectory()) {
+        if(fileCredentials.exists() && !fileCredentials.isDirectory()) {
             MainModel.getInstance().setText("Credential Exsist");
 
 
         } else {
             MainModel.getInstance().setText("Credential Not Found");
             MainModel.getInstance().setText("You should enter new Credentials's");
+        }
+
+
+        if(fileBucket.exists() && !fileBucket.isDirectory()) {
+            MainModel.getInstance().setText("Bucket Name Exsist");
+
+
+        } else {
+            MainModel.getInstance().setText("Backet Name Not Found");
+            MainModel.getInstance().setText("You should enter new BucketName");
         }
 
     }
@@ -68,14 +90,19 @@ public class SettingsController {
 
     @FXML public void applyAction(ActionEvent event) throws IOException {
 
-        file.getParentFile().mkdirs();
-        file.createNewFile();
+        fileCredentials.getParentFile().mkdirs();
+        fileCredentials.createNewFile();
 
-        PrintWriter writer = new PrintWriter(path, "UTF-8");
+        PrintWriter writer = new PrintWriter(pathCredentials, "UTF-8");
         writer.println("[default]");
         writer.println("aws_access_key_id = " + accessKeyField.getText());
         writer.println("aws_secret_access_key = " + securityKeyField.getText());
         writer.close();
+
+        PrintWriter bucketWriter = new PrintWriter(pathBucket, "UTF-8");
+        bucketWriter.println(bucketName.getText());
+        bucketWriter.close();
+
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
 
